02fa2054bfc3604e4af20421ff747b0b1722cd76
Sanjeetkumarrao <sanjeetkumarbharti2018@gmail.com>
2026-06-09 09:34:51 +0530

    updated url

--- minitube/backend/app.js
+++ minitube/backend/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(
   cors({
     // origin: "http://localhost:5173",
-    origin: "minitube-ab6n980zo-sanjeetkumarraos-projects.vercel.app",
+    origin: "https://minitube-ab6n980zo-sanjeetkumarraos-projects.vercel.app",
     credentials: true,
   })
 );
